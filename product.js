2f8b0cc336bc0eaa49c2a1e59dacb63d64e63d33
Sagar Rana <117368325+ssagar9495@users.noreply.github.com>
2022-12-16 17:44:06 +0530

    blinkIy backend jason file

---
+++ product.js
@@ -0,0 +1,477 @@
+export const products = [
+  {
+    catergory_id: 1,
+    catergory_name: 'dariy',
+    item_name: 'milk',
+    products: [
+      {
+        product_name: 'verka standard Fresh Milk',
+        product_id: '100',
+        product_quatity: '500 ml',
+        product_proce: '29',
+      },
+      {
+        product_name: 'verka Doublec Toned Fresh Milk',
+        product_id: '101',
+        product_quatity: '500 ml',
+        product_proce: '23',
+      },
+    ],
+  },
+  {
+    catergory_id: 1,
+    catergory_name: 'dariy',
+    item_name: 'bread',
+    products: [
+      {
+        product_name: 'bonn high fiber brown bread',
+        product_id: '111',
+        product_quatity: '400 ml',
+        product_proce: '45',
+      },
+      {
+        product_name: 'bonn atta whole wheat bread',
+        product_id: '112',
+        product_quatity: '400 ml',
+        product_proce: '45',
+      },
+    ],
+  },
+  {
+    catergory_id: 1,
+    catergory_name: 'dariy',
+    item_name: 'eggs',
+    products: [
+      {
+        product_name: 'table white eggs (6 pieces)',
+        product_id: '121',
+        product_quatity: '1 pack (6 pieces)',
+        product_proce: '58',
+      },
+      {
+        product_name: 'table white eggs - 30 pieces',
+        product_id: '122',
+        product_quatity: '30 pieces ',
+        product_proce: '262',
+      },
+    ],
+  },
+  {
+    catergory_id: 1,
+    catergory_name: 'dariy',
+    item_name: 'poha',
+    products: [
+      {
+        product_name: 'rajdhani thick poha',
+        product_id: '131',
+        product_quatity: '500 g',
+        product_proce: '57',
+      },
+      {
+        product_name: 'table white eggs - 30 pieces',
+        product_id: '132',
+        product_quatity: '500 gm',
+        product_proce: '40',
+      },
+    ],
+  },
+  {
+    catergory_id: 1,
+    catergory_name: 'dariy',
+    item_name: 'butter',
+    products: [
+      {
+        product_name: 'amul saled butter',
+        product_id: '141',
+        product_quatity: '100 g',
+        product_proce: '54',
+      },
+      {
+        product_name: 'verka saled butter',
+        product_id: '142',
+        product_quatity: '100 gm',
+        product_proce: '54',
+      },
+    ],
+  },
+  {
+    catergory_id: 2,
+    catergory_name: 'vegetables',
+    item_name: 'veg',
+    products: [
+      {
+        product_name: 'potato',
+        product_id: '200',
+        product_quatity: '1 kg',
+        product_proce: '27',
+      },
+      {
+        product_name: 'onion',
+        product_id: '201',
+        product_quatity: '1 kg ',
+        product_proce: '23',
+      },
+    ],
+  },
+  {
+    catergory_id: 2,
+    catergory_name: 'vegetables',
+    item_name: 'fruits',
+    products: [
+      {
+        product_name: 'banana',
+        product_id: '211',
+        product_quatity: '12 pieces',
+        product_proce: '59',
+      },
+      {
+        product_name: 'apple',
+        product_id: '212',
+        product_quatity: '1 kg',
+        product_proce: '40',
+      },
+    ],
+  },
+  {
+    catergory_id: 2,
+    catergory_name: 'seasonal',
+    item_name: 'potato',
+    products: [
+      {
+        product_name: 'carrot',
+        product_id: '221',
+        product_quatity: '1 kg',
+        product_proce: '39',
+      },
+      {
+        product_name: 'brocli',
+        product_id: '222',
+        product_quatity: '1 kg',
+        product_proce: '103',
+      },
+    ],
+  },
+  {
+    catergory_id: 2,
+    catergory_name: 'vegetables',
+    item_name: 'exotics',
+    products: [
+      {
+        product_name: 'reg graphes',
+        product_id: '231',
+        product_quatity: '200g',
+        product_proce: '129',
+      },
+      {
+        product_name: 'red capsicum',
+        product_id: '232',
+        product_quatity: '1 piece',
+        product_proce: '25',
+      },
+    ],
+  },
+  {
+    catergory_id: 2,
+    catergory_name: 'vegetables',
+    item_name: 'organic',
+    products: [
+      {
+        product_name: 'beetroot',
+        product_id: '241',
+        product_quatity: '250 g',
+        product_proce: '36',
+      },
+      {
+        product_name: 'bottle gourd',
+        product_id: '242',
+        product_quatity: '1 piece',
+        product_proce: '53',
+      },
+    ],
+  },
+  {
+    catergory_id: 3,
+    catergory_name: 'munchies',
+    item_name: 'chips',
+    products: [
+      {
+        product_name: 'kukure',
+        product_id: '301',
+        product_quatity: '82 g',
+        product_proce: '38',
+      },
+      {
+        product_name: 'bingo',
+        product_id: '302',
+        product_quatity: '82 g',
+        product_proce: '76',
+      },
+    ],
+  },
+  {
+    catergory_id: 3,
+    catergory_name: 'munchies',
+    item_name: 'nacho',
+    products: [
+      {
+        product_name: 'cheese nacho',
+        product_id: '311',
+        product_quatity: '66 g',
+        product_proce: '38',
+      },
+      {
+        product_name: 'salsa combo',
+        product_id: '312',
+        product_quatity: '45g',
+        product_proce: '59',
+      },
+    ],
+  },
+  {
+    catergory_id: 3,
+    catergory_name: 'munchies',
+    item_name: 'popcorn',
+    products: [
+      {
+        product_name: 'act II golden popcorn',
+        product_id: '321',
+        product_quatity: '30 g',
+        product_proce: '10',
+      },
+      {
+        product_name: 'act II butter popcorn ',
+        product_id: '322',
+        product_quatity: '30 g',
+        product_proce: '40',
+      },
+    ],
+  },
+  {
+    catergory_id: 3,
+    catergory_name: 'munchies',
+    item_name: 'bhujiya',
+    products: [
+      {
+        product_name: 'haldiram punjabi tadka',
+        product_id: '331',
+        product_quatity: '220 g',
+        product_proce: '36',
+      },
+      {
+        product_name: 'haldiram navratan',
+        product_id: '331',
+        product_quatity: '220 g',
+        product_proce: '53',
+      },
+    ],
+  },
+  {
+    catergory_id: 3,
+    catergory_name: 'munchies',
+    item_name: 'namkeen',
+    products: [
+      {
+        product_name: 'tasty Peanut',
+        product_id: '341',
+        product_quatity: '220 g',
+        product_proce: '50',
+      },
+      {
+        product_name: 'nut cracker',
+        product_id: '342',
+        product_quatity: '220 g',
+        product_proce: '50',
+      },
+    ],
+  },
+  {
+    catergory_id: 4,
+    catergory_name: 'cold drinks & juices',
+    item_name: 'soft drinks',
+    products: [
+      {
+        product_name: 'coca cola',
+        product_id: '401',
+        product_quatity: '1 bottle',
+        product_proce: '50',
+      },
+      {
+        product_name: 'dew',
+        product_id: '402',
+        product_quatity: '1 bottle',
+        product_proce: '50',
+      },
+    ],
+  },
+  {
+    catergory_id: 4,
+    catergory_name: 'cold drinks & juices',
+    item_name: 'fruit juices',
+    products: [
+      {
+        product_name: 'maaza mango',
+        product_id: '411',
+        product_quatity: '1 bottle',
+        product_proce: '50',
+      },
+      {
+        product_name: 'real fruit juice',
+        product_id: '412',
+        product_quatity: '1 bottle',
+        product_proce: '50',
+      },
+    ],
+  },
+  {
+    catergory_id: 4,
+    catergory_name: 'cold drinks & juices',
+    item_name: 'cold press',
+    products: [
+      {
+        product_name: 'real activ 100%',
+        product_id: '421',
+        product_quatity: '1 bottle',
+        product_proce: '50',
+      },
+      {
+        product_name: 'paper boat coocnut water',
+        product_id: '422',
+        product_quatity: '1 bottle',
+        product_proce: '50',
+      },
+    ],
+  },
+  {
+    catergory_id: 4,
+    catergory_name: 'cold drinks & juices',
+    item_name: 'energy drink',
+    products: [
+      {
+        product_name: 'string energy drink',
+        product_id: '431',
+        product_quatity: '1 bottle',
+        product_proce: '50',
+      },
+      {
+        product_name: 'red bull energy drink',
+        product_id: '431',
+        product_quatity: '1 bottle',
+        product_proce: '50',
+      },
+    ],
+  },
+  {
+    catergory_id: 4,
+    catergory_name: 'cold drinks & juices',
+    item_name: 'water',
+    products: [
+      {
+        product_name: 'bislery packed water',
+        product_id: '441',
+        product_quatity: '1 bottle',
+        product_proce: '50',
+      },
+      {
+        product_name: 'kinley packed water',
+        product_id: '442',
+        product_quatity: '1 bottle',
+        product_proce: '50',
+      },
+    ],
+  },
+  {
+    catergory_id: 5,
+    catergory_name: 'bakery',
+    item_name: 'cookies',
+    products: [
+      {
+        product_name: 'parle g',
+        product_id: '501',
+        product_quatity: '200 gm',
+        product_proce: '50',
+      },
+      {
+        product_name: 'good day',
+        product_id: '502',
+        product_quatity: '200 gm',
+        product_proce: '50',
+      },
+    ],
+  },
+  {
+    catergory_id: 5,
+    catergory_name: 'bakery',
+    item_name: 'cream biscuits',
+    products: [
+      {
+        product_name: 'oreo',
+        product_id: '511',
+        product_quatity: '200 gm',
+        product_proce: '50',
+      },
+      {
+        product_name: 'sun feast',
+        product_id: '512',
+        product_quatity: '200 gm',
+        product_proce: '50',
+      },
+    ],
+  },
+  {
+    catergory_id: 5,
+    catergory_name: 'bakery',
+    item_name: 'bread pav',
+    products: [
+      {
+        product_name: 'boon bread pav',
+        product_id: '521',
+        product_quatity: '200 gm',
+        product_proce: '50',
+      },
+      {
+        product_name: 'lotus bread pav',
+        product_id: '522',
+        product_quatity: '200 gm',
+        product_proce: '50',
+      },
+    ],
+  },
+  {
+    catergory_id: 5,
+    catergory_name: 'bakery',
+    item_name: 'glucose',
+    products: [
+      {
+        product_name: 'pitanjali glucose',
+        product_id: '531',
+        product_quatity: '200 gm',
+        product_proce: '50',
+      },
+      {
+        product_name: 'birtania maria gold',
+        product_id: '531',
+        product_quatity: '200 gm',
+        product_proce: '50',
+      },
+    ],
+  },
+  {
+    catergory_id: 5,
+    catergory_name: 'bakery',
+    item_name: 'rusks',
+    products: [
+      {
+        product_name: 'britania rusks',
+        product_id: '541',
+        product_quatity: '200 gm',
+        product_proce: '50',
+      },
+      {
+        product_name: 'parle butter rusks',
+        product_id: '542',
+        product_quatity: '200 gm',
+        product_proce: '50',
+      },
+    ],
+  },
+];
